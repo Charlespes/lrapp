13945756ab87b7f5351b114e809a9847f3f312ba
Charles Chan <Charles Chan>
2016-11-04 17:16:53 +0800

    configured DB settings

--- lrapp/settings.py
+++ lrapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lrapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lrappDB',
+        'USER': 'postgres',
+        'PASSWORD': '1q2w3e',
+        'HOST': '/tmp',
+        'PORT': '5432',
     }
 }
 
